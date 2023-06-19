d92c390e563ffa8f7a3cdeded455853c1a793988
Ajit kumar <kajit0408@gmail.com>
2023-06-19 22:51:47 +0530

    routes and components initialised

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,22 @@
-// import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./components/nav";
+import Footer from "./components/footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     <h1>E-commerce</h1>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<h1>This is product listing</h1>} />
+          <Route path="/add" element={<h1>This is add product</h1>} />
+          <Route path="/update" element={<h1>This is update product</h1>} />
+          <Route path="/logout" element={<h1>This is logout</h1>} />
+          <Route path="/profile" element={<h1>This is profile</h1>} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
