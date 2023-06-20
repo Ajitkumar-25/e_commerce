f8db1644e5fe7c673935f5670fe3b2e1703167e4
Ajit kumar <kajit0408@gmail.com>
2023-06-20 22:17:57 +0530

    logout feature done

--- front-end/src/components/nav.js
+++ front-end/src/components/nav.js
@@ -1,7 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 const Nav = () => {
   const auth=localStorage.getItem('user');
+  const navigate=useNavigate();
+
+// logout feature it is just clearing local storage 
+  const logout=()=>{
+    localStorage.clear();
+    navigate('/')
+  }
+
+
   return <div>
     <ul className="nav-ul">
         <li><Link to="/">Products</Link></li>
@@ -9,7 +18,7 @@ const Nav = () => {
         <li><Link to="/update"> Update Products</Link></li>
         <li><Link to="/profile">Profile</Link></li>
         {/* agar user sign in hai to logout ka option dega varna sign up ka  */}
-        <li>{auth?<Link to="/logout">Logout</Link>:<Link to="/signup">Signup</Link>}</li>
+        <li>{auth?<Link onClick={logout} to="/signup">Logout</Link>:<Link to="/signup">Signup</Link>}</li>
         
     </ul>
   </div>;
