0f6c45d474a1296feb687ff48fb51e08f63e75d2
Ajit kumar <kajit0408@gmail.com>
2023-06-21 21:34:30 +0530

    navbar updated

--- front-end/src/components/nav.js
+++ front-end/src/components/nav.js
@@ -1,38 +1,52 @@
 import React from "react";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const Nav = () => {
-  const auth=localStorage.getItem('user');
-  const navigate=useNavigate();
+  const auth = localStorage.getItem("user");
+  const navigate = useNavigate();
 
-// logout feature it is just clearing local storage 
-  const logout=()=>{
+  // logout feature it is just clearing local storage
+  const logout = () => {
     localStorage.clear();
-    navigate('/')
-  }
+    navigate("/");
+  };
 
-
-  return <div>
-    <ul className="nav-ul">
-        <li><Link to="/">Products</Link></li>
-        <li><Link to="/add"> Add Products</Link></li>
-        <li><Link to="/update"> Update Products</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
-        {/* agar user sign in hai to logout ka option dega varna sign up ka  */}
-
-        {/* <li>{auth?<Link onClick={logout} to="/signup">Logout</Link>:<Link to="/signup">Signup</Link>}</li>
-        <li><Link to={"/login"}>Login</Link></li> */}
-
-        {/* ye maine upar isliye comment kiya haim kyuki baad me neeche vala code likha tha
-        jiske hisaab se yadi login hai to sirf logout ka optyion dega */}
-
-        {
-          auth?<li><Link onClick={logout} to="/signup">Logout</Link></li>
-          :<>
-          <li><Link to="/signup">Signup</Link></li>
-          <li><Link to={"/login"}>Login</Link></li>
-          </>
-}
-    </ul>
-  </div>;
+  return (
+    <div>
+      <img
+        src="https://th.bing.com/th/id/OIP.O5ip3Cal9h_8WUar84JeMAHaFP?w=214&h=180&c=7&r=0&o=5&pid=1.7"
+        alt="hula" className="logo"
+      />
+      {auth ? (
+        <ul className="nav-ul">
+          <li>
+            <Link to="/">Products</Link>
+          </li>
+          <li>
+            <Link to="/add"> Add Products</Link>
+          </li>
+          <li>
+            <Link to="/update"> Update Products</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link onClick={logout} to="/signup">
+              Logout ({JSON.parse(auth).name})
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <ul className="nav-ul nav-right">
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to={"/login"}>Login</Link>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
 };
 export default Nav;
