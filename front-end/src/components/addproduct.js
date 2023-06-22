2e3bbe5b5d4fc331376fa30e970d0d169c5f1062
Ajit kumar <kajit0408@gmail.com>
2023-06-22 14:32:40 +0530

    validation and error message added

--- front-end/src/components/addproduct.js
+++ front-end/src/components/addproduct.js
@@ -1,25 +1,30 @@
 import React from "react";
 import { useState } from "react";
 const Addproduct = () => {
+  const [name, setname] = useState("");
+  const [price, setprice] = useState("");
+  const [category, setcategory] = useState("");
+  const [company, setcompany] = useState("");
+  const [error, seterror] = useState(false);
 
-    const [name, setname] = useState("");
-    const [price, setprice] = useState("");
-    const [category, setcategory] = useState("");
-    const [company, setcompany] = useState("");
+  const handleaddproduct = async () => {
+    if (!name || !price || !category || !company) {
+      seterror(true);
+      return false;
+    }
 
-const handleaddproduct= async()=>{
-    console.warn(name,price,category,company);
-    const userid=JSON.parse(localStorage.getItem('user'))._id;
-    let result=await fetch("http://localhost:5000/add-product",{
-        method:"POST",
-        body:JSON.stringify({name,price,category,company,userid}),
-        headers:{
-            "Content-Type":"application/json"
-        }
+    console.warn(name, price, category, company);
+    const userid = JSON.parse(localStorage.getItem("user"))._id;
+    let result = await fetch("http://localhost:5000/add-product", {
+      method: "POST",
+      body: JSON.stringify({ name, price, category, company, userid }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
-    result=await result.json();
+    result = await result.json();
     console.warn(result);
-}
+  };
 
   return (
     <div className="addproduct">
@@ -28,30 +33,56 @@ const handleaddproduct= async()=>{
         type="text"
         className="input-box"
         value={name}
-        onChange={(e)=>{setname(e.target.value)}}
+        onChange={(e) => {
+          setname(e.target.value);
+        }}
         placeholder="Enter product name"
       />
+      {error && !name && (
+        <span className="invalid-input">Please enter valid name</span>
+      )}
+
       <input
         type="text"
         className="input-box"
         value={price}
-        onChange={(e)=>{setprice(e.target.value)}}
+        onChange={(e) => {
+          setprice(e.target.value);
+        }}
         placeholder="Enter product price"
       />
+      {error && !price && (
+        <span className="invalid-input">Please enter valid price</span>
+      )}
+
       <input
         type="text"
         className="input-box"
         value={category}
-        onChange={(e)=>{setcategory(e.target.value)}}
+        onChange={(e) => {
+          setcategory(e.target.value);
+        }}
         placeholder="Enter product category"
       />
+
+      {error && !category && (
+        <span className="invalid-input">Please enter valid category</span>
+      )}
+
       <input
         type="text"
         className="input-box"
         value={company}
-        onChange={(e)=>{setcompany(e.target.value)}}
+        onChange={(e) => {
+          setcompany(e.target.value);
+        }}
         placeholder="Enter product company"
       />
+
+      {error && !company && (
+        <span className="invalid-input">Please enter valid company</span>
+      )}
+
       <button onClick={handleaddproduct}>Add product</button>
     </div>
   );
